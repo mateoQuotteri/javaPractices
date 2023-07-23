1f994a3b4454b31c0a1bad22819ae776185de91a
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-23 18:08:37 -0300

    first java file (practice functions)

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
 package com.company;
 
+import 
+/*
 public class Main{
     public static void main(String[] args){
         System.out.println("Hola")
     }
+}*/
+
+public class Main {
+
+    public boolean esDivisible(int numero1,int numero2) {
+	int resto = numero1 % numero2;
+
+    if(resto == 0){
+        return true;
+    }else{
+        return false;
+    }
+
+
+    }
+
+    
 }
